c60bfd74d4c337ad6f4c59be6570e43c713844f7
aiworld9010-tech <aiworld9010@gmail.com>
2026-07-01 02:56:10 +0400

    Include native folders in git

---
+++ android/app/.cxx/Debug/4fu3j6a1/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/fbjni/fbjniConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET fbjni::fbjni)
+add_library(fbjni::fbjni SHARED IMPORTED)
+set_target_properties(fbjni::fbjni PROPERTIES
+    IMPORTED_LOCATION "C:/Users/futur/.gradle/caches/9.3.1/transforms/87ff0c707cf9d806e3b45f072d12c5f5/workspace/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.arm64-v8a/libfbjni.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/futur/.gradle/caches/9.3.1/transforms/87ff0c707cf9d806e3b45f072d12c5f5/workspace/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
